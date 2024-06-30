670e905e60df2377e4c992811a0a8b5623ddbb4a
Ridwat <okunlolaridwanat@gmail.com>
2024-06-30 20:22:35 +0100

    edited

--- src/Pages/Home/Component/Section1.jsx
+++ src/Pages/Home/Component/Section1.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import "./Section1.css"
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 const Section1 = () => {
   const { t } = useTranslation();
