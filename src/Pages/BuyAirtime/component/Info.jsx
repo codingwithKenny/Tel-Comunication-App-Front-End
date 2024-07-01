cf1bf97838dfa938ac6d759aaac820c3bbf7ca8f
Ridwat <okunlolaridwanat@gmail.com>
2024-07-01 09:45:34 +0100

    edited

--- src/Pages/BuyAirtime/component/Info.jsx
+++ src/Pages/BuyAirtime/component/Info.jsx
@@ -4,8 +4,8 @@ const Info = () => {
   return (
     // <div className="bg-gray-100 p-4">
         <div className="bg-white rounded-lg shadow-md p-4">
-          <p className="text-gray-600 text-sm mb-2">Must include Country code</p>
-          <p className="text-gray-600 text-xm mb-4">Format: 2348106114218</p>
+          <p className="text-gray-600 text-xs mb-2">Must include Country code</p>
+          <p className="text-gray-600 text-xs mb-4">Format: 2348106114218</p>
       </div>
     // </div>
   );
