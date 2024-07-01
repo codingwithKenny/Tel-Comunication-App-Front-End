62951f63d68de98012aaf276341520d47aee368d
Ridwat <okunlolaridwanat@gmail.com>
2024-07-01 09:43:52 +0100

    edited

--- src/Pages/BuyAirtime/component/Info.jsx
+++ src/Pages/BuyAirtime/component/Info.jsx
@@ -5,7 +5,7 @@ const Info = () => {
     // <div className="bg-gray-100 p-4">
         <div className="bg-white rounded-lg shadow-md p-4">
           <p className="text-gray-600 text-sm mb-2">Must include Country code</p>
-          <p className="text-gray-600 text-sm mb-4">Format: 2348106114218</p>
+          <p className="text-gray-600 text-xm mb-4">Format: 2348106114218</p>
       </div>
     // </div>
   );
