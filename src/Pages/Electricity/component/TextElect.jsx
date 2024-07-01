abcd08a5dd9654de5a9ed5113057a1c4f05cab16
Ridwat <okunlolaridwanat@gmail.com>
2024-07-01 10:04:51 +0100

    edited

---
+++ src/Pages/Electricity/component/TextElect.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import"/TextElect.css"
+
+const TextElect = () => {
+  return (
+    <div>
+       <h1 className='text'>Electricty</h1>
+    </div>
+  )
+}
+
+export default TextElect
