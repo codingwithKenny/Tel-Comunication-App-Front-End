f44091e231abb21186aea4ddec4f79504dd71ee0
Ridwat <okunlolaridwanat@gmail.com>
2024-07-01 09:40:14 +0100

    edited

--- src/Pages/BuyAirtime/component/Info.jsx
+++ src/Pages/BuyAirtime/component/Info.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Info = () => {
   return (
     <div className="bg-gray-100 p-4">
-        <div className="bg-white rounded-lg shadow-md p-4">
-          <p className="text-gray-600 text-sm mb-2">Must include Country code</p>
-          <p className="text-gray-600 text-sm mb-4">Format: 2348106114218</p>
-      </div>
+        {/* <div className="bg-white rounded-lg shadow-md p-4"> */}
+          <p className="text-yellow-600 text-sm mb-2">Must include Country code</p>
+          <p className="text-yellow-600 text-sm mb-4">Format: 2348106114218</p>
+      {/* </div> */}
     </div>
   );
 };
