08ae1150f47e12b0d11392918cf6f449fe8923fa
Ridwat <okunlolaridwanat@gmail.com>
2024-07-03 23:28:43 +0100

    correction

--- src/Pages/BuyData/utils/data.js
+++ src/Pages/BuyData/utils/data.js
@@ -11,7 +11,7 @@ export const DataInput = [
         name: "network",
         type: "text",
         required: true,
-        // readOnly: true
+        readOnly: true
     },
     {
         label: 'Plan',
